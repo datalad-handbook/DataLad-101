9723a808cfad550585bc26296a4fce3b128ce38a
Elena Piscopia <elena@example.net>
2020-03-17 08:59:56 +0100

    BF: list both directories content

--- code/list_titles.sh
+++ code/list_titles.sh
@@ -1,10 +1,10 @@
-for i in recordings/longnow/Long_Now__Seminars*/*.mp3; do
+for i in recordings/longnow/Long_Now*/*.mp3; do
    # get the filename
    base=$(basename "$i");
    # strip the extension
    base=${base%.mp3};
-   # date as yyyy-mm-dd
    printf "${base%%__*}\t" | tr '_' '-';
    # name and title without underscores
    printf "${base#*__}\n" | tr '_' ' ';
+
 done
